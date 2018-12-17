dac1e5ce7c984829dbe8c93e7c8ccee372fbb86b
killdom2 <45922339+killdom2@users.noreply.github.com>
2018-12-17 22:54:32 +0400

    create New_Calculate

---
+++ New_Calculate.java
@@ -0,0 +1,14 @@
+class New_Calculate{
+
+  public static void main(String[] args){
+
+    double x = Double.valueOf (args[0]);
+    double y = Double.valueOf (args[1]);
+    System.out.println("sum = " + (x + y));
+    /*double a = (double)(x);
+    double b = (double)(y);*/
+    char ch = '\t';
+    System.out.println("div = " + (x / y));
+    System.out.println("x" + ch + "y");
+  }
+}
